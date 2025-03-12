41ab7f56f4d44729b6322c79299de3aa2bf8abec
cyh0429 <101920074+cyh0429@users.noreply.github.com>
2025-03-13 01:08:17 +1030

    clustervis code.R

--- clustervis.R
+++ clustervis.R
@@ -16,9 +16,9 @@ library(data.table)
 library(ggplot2)
 library(ggpubr)
 library(Cairo)
+
 data<-read.table(file='path/data.csv',header=TRUE,row.names= 1,sep=',')
 head(data,3)
-#vis
 data<-data
 set.seed(123456)
 getClusters(exp = data)
@@ -27,7 +27,6 @@ cm <- clusterData(exp = data,
                   cluster.method = "mfuzz",
                   cluster.num = 4)
 str(cm)
-CairoPDF("char.pdf", width =16, height =4)
 visCluster(object = cm, 
            plot.type = "line",
            ms.col = c("green","orange","blue"),
@@ -50,17 +49,10 @@ visCluster(object = cm,
       size=12
     ))+
   theme(axis.ticks.length = unit(-0.15,'cm'))
-dev.off()
-# plot line 
-# remove meadian line
 dir.create(path="mfu")
 cc<- cm$wide.res
 write.csv(cc,paste0("mfu","/mfuzz",".csv"))
-library(extrafont)
-font_import(path = "C:/Windows/Fonts", pattern = "times", prompt = FALSE)
-loadfonts(device = "pdf")
 library(grid)
-pdf('testbox2.pdf',height = 6,width = 6)
 visCluster(object = cm,
            plot.type = "both",
            column_names_rot = 0,
@@ -69,5 +61,31 @@ visCluster(object = cm,
            line.side = "left",
            show_row_dend = F,
            boxcol = ggsci::pal_npg()(8) )            
-
+#kmeans
+ck <- clusterData(exp = data,
+                  cluster.method = "kmeans",
+                  cluster.num = 4)
+str(cm)
+visCluster(object = ck, 
+           plot.type = "line",
+           ms.col = c("blue","yellow","red"),
+           add.mline = T)+
+  theme(axis.title = element_text(
+    family = 'Times New Roman',
+    face='bold', 
+    size=15, 
+    lineheight = 1),
+    axis.text.x = element_text(
+      family = 'Times New Roman',
+      face="bold", 
+      color="black",
+      size=12,
+      angle = 0),
+    axis.text.y = element_text(
+      family = 'Times New Roman',
+      face="bold", 
+      color="black",
+      size=12
+    ))+
+  theme(axis.ticks.length = unit(-0.15,'cm'))
 dev.off()
